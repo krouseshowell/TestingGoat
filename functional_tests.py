6f67b559a043d1a260a86e970ce4a9acc02d0b11
kendrarose <kendrarose@grdylan.net>
2017-07-11 14:59:51 -0700

    about to refactor

--- functional_tests.py
+++ functional_tests.py
@@ -35,14 +35,23 @@ class NewVisitorTest(unittest.TestCase):
 #The item on the to-do lists
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #there is still a text book
+        #enters another item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        #page updates and shows both items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
-#there is still a text book
-#enters another item.
+        #page updates and shows both items
         self.fail('Finish the test!')
-#page updates and shows both items
 #the site has generated a unique url and there is explanitory test
 #visits that url -todo list is there
 #leaves
