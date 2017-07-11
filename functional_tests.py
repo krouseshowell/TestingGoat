0dc35bcd92a24f5e5d5ccb5a817bfd9bdb5bb8f8
kendrarose <kendrarose@grdylan.net>
2017-07-10 18:51:10 -0700

    Add app for lists, with deliberately failing unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def  setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #There is a site, go to it!
+        self.browser.get('http://localhost:8000')
+
+        #Does the title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+#There is a place to enter a to do item.
+#Type a item inem into the test box
+#when she hits enter, the page updates and the page lists:
+#The item on the to-do lists
+#there is still a text book
+#enters another item.
+#page updates and shows both items
+#the site has generated a unique url and there is explanitory test
+#visits that url -todo list is there
+#leaves
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
