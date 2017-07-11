4edab627a55d5370e634dd9c2902c14f69b1e0fb
kendrarose <kendrarose@grdylan.net>
2017-07-11 09:36:20 -0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,15 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 
         #Does the title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assrtIn('To-Do', header_text)
+
+        #There is a place to enter a to do item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        #Type a "Buy peacock feathers" into the test box
+        imput.send_keys('Buy peacock feathers')
 
 
-#There is a place to enter a to do item.
-#Type a item inem into the test box
-#when she hits enter, the page updates and the page lists:
+        #when she hits enter, the page updates and the page lists:
+        inputbox.send_keys(keys.ENTER)
+        time.sleep(1)
 #The item on the to-do lists
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 #there is still a text book
 #enters another item.
+        self.fail('Finish the test!')
 #page updates and shows both items
 #the site has generated a unique url and there is explanitory test
 #visits that url -todo list is there
